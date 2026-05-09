cf91e31b56d56fbad14ae09f662d9864325e1bfc
Ben White <benwhite1987@gmail.com>
2026-05-09 12:41:26 -0500

    Refactor Valheim Discord Event Logger
    Refactored the Valheim server event logger to improve readability and functionality. Updated event processing functions and added state tracking for events.

--- discordevent.sh
+++ discordevent.sh
@@ -1,139 +1,226 @@
 #!/bin/bash
 
-# LGSM Valheim Server log.  Default username is 'vhserver' and default log location is below.
+################################################################################
+# LGSM Valheim Server Event Logger - Refactored
+# Monitors server log and sends Discord notifications for events
+################################################################################
+
+# Configuration
 SERVERLOG="/home/vhserver/log/console/vhserver-console.log"
-# Replace with your own discord server webhook
 DISCORDWEBHOOK="==YOUR DISCORD WEBHOOK HERE=="
-# Make our own logfile for debug and see historic data during development.  Script is pretty verbose and echos to stdout for systemd too because I'm lazy
 LOGFILE="/home/vhserver/log/dea.log"
 
-LOGTIME="[$(date +"%Y%b%d %H:%M:%S")]"
-
+# State tracking
 declare -A players
+declare -A raid_last_time
+declare -A death_last_time
+last_processed_line=0
 
-# Random events names as defined by Valheim wiki at https://valheim.fandom.com/wiki/Events
-declare -A raids
-raids[army_eikthyr]=":deer::boar: Eikthyr rallies the creatures of the forest against you! :boar::deer:"
-raids[army_theelder]=":evergreen_tree::wood: The forest is moving with blue eyes gleaming! :wood::evergreen_tree:"
-raids[army_bonemass]=":pirate_flag: A foul smell from the swamp brings the undead to your door! :pirate_flag:"
-raids[army_moder]=":cloud_snow: A cold wind blows from the mountains as the skies fill with screeching! :snowflake:"
-raids[army_goblin]=":smiling_imp: The Fuling horde is hell bent on destruction! :smiling_imp:"
-raids[army_gjall]=":beetle::fire: Gjall reigns his ticks and bile upon your dwelling! :fire::beetle:"
-raids[army_seekers]=":fly: They seek those that threaten their Queen! :fly:"
-raids[army_charred]=":skull::hot_face: The undead army of ash marches. :hot_face::skull"
-raids[army_charredspawners]=":skull::hot_face: The ashen undead have been summoned! :hot_face::skull"
-raids[foresttrolls]=":troll: The ground is shaking as big blue bastards begin breaking shit! :troll:"
-raids[blobs]=":microbe: A foul smell from the swamp brings green blobs! :microbe:"
-raids[ghosts]=":ghost: You feel a chill down your spine... :ghost:"
-raids[skeletons]=":skull_crossbones: There's a Skeleton Surprise!  Time to break some bones! :skull_crossbones:"
-raids[surtlings]=":fire: There's a smell of sulfur in the air as surtlings reign fire! :fire:"
-raids[wolves]=":wolf: You are being hunted by wolves! :wolf:"
-raids[bats]=":bat: Batting wings, shrieks, and fangs whirl around your home! :bat:"
-raids[hildirboss1]=":fire::skull_crossbones: Brenna seeks her fiery revenge! :skull_crossbones::fire:"
-raids[hildirboss2]=":cold_face: Geirrhafa seeks his chilly revenge! :cold_face:"
-raids[hildirboss3]=":imp::mage: Zil and Thungr seek their brotherly revenge! :mage::imp:"
+################################################################################
+# Event Definitions
+################################################################################
 
-echo "$LOGTIME Starting Valheim Discord Event Log" >> $LOGFILE
-init () {
-	if [ -e $SERVERLOG ]
-	then
-		echo "$LOGTIME Found Server Log" >> $LOGFILE
-		readlog
+declare -A raids=(
+	[army_eikthyr]=":deer::boar: Eikthyr rallies the creatures of the forest against you! :boar::deer:"
+	[army_theelder]=":evergreen_tree::wood: The forest is moving with blue eyes gleaming! :wood::evergreen_tree:"
+	[army_bonemass]=":pirate_flag: A foul smell from the swamp brings the undead to your door! :pirate_flag:"
+	[army_moder]=":cloud_snow: A cold wind blows from the mountains as the skies fill with screeching! :snowflake:"
+	[army_goblin]=":smiling_imp: The Fuling horde is hell bent on destruction! :smiling_imp:"
+	[army_gjall]=":beetle::fire: Gjall reigns his ticks and bile upon your dwelling! :fire::beetle:"
+	[army_seekers]=":fly: They seek those that threaten their Queen! :fly:"
+	[army_charred]=":skull::hot_face: The undead army of ash marches. :hot_face::skull"
+	[army_charredspawners]=":skull::hot_face: The ashen undead have been summoned! :hot_face::skull"
+	[foresttrolls]=":troll: The ground is shaking as big blue bastards begin breaking shit! :troll:"
+	[blobs]=":microbe: A foul smell from the swamp brings green blobs! :microbe:"
+	[ghosts]=":ghost: You feel a chill down your spine... :ghost:"
+	[skeletons]=":skull_crossbones: There's a Skeleton Surprise!  Time to break some bones! :skull_crossbones:"
+	[surtlings]=":fire: There's a smell of sulfur in the air as surtlings reign fire! :fire:"
+	[wolves]=":wolf: You are being hunted by wolves! :wolf:"
+	[bats]=":bat: Batting wings, shrieks, and fangs whirl around your home! :bat:"
+	[hildirboss1]=":fire::skull_crossbones: Brenna seeks her fiery revenge! :skull_crossbones::fire:"
+	[hildirboss2]=":cold_face: Geirrhafa seeks his chilly revenge! :cold_face:"
+	[hildirboss3]=":imp::mage: Zil and Thungr seek their brotherly revenge! :mage::imp:"
+)
+
+################################################################################
+# Utility Functions
+################################################################################
+
+log_script() {
+	local timestamp="[$(date +"%Y%b%d %H:%M:%S")]"
+	echo "$timestamp $*" | tee -a "$LOGFILE"
+}
+
+log_file_only() {
+	local timestamp="[$(date +"%Y%b%d %H:%M:%S")]"
+	echo "$timestamp $*" >> "$LOGFILE"
+}
+
+send_discord() {
+	local username="$1"
+	local message="$2"
+	local json="{\"username\": \"$username\", \"content\": \"$message\"}"
+	
+	if curl --connect-timeout 10 -sSL -H "Content-Type: application/json" \
+		-X POST -d "$json" "$DISCORDWEBHOOK" 2>/dev/null; then
+		log_file_only "Discord sent: $username - $message"
 	else
-		echo "$LOGTIME Server Log NOT Found, Exiting Now" >> $LOGFILE
-		return 1
+		log_script "ERROR: Failed to send Discord message: $message"
 	fi
 }
 
-readlog () {
-	# Set all initial variables to null
-	LAST=""
-	LATEST=""
-	LASTRAID=""
-	LASTDEATH=""
-	while true; do
- 		LOGTIME="[$(date +"%Y%b%d %H:%M:%S")]"
-		# Get latest information from LGSM Valheim server log and check to see if there are new events
-		LATEST=$(tail -n 10 $SERVERLOG)
-		if [[ $LATEST != $LAST ]]
-		then
-			# RegEx to pull a Raid Event line, parse event variable, log time to prevent repeat messages, and send Discord message
-			RAIDLINE=$(echo $LATEST | grep -oP '(\d{2}\/\d{2}\/\d{4}\s\d{2}\:\d{2}\:\d{2}\: Random event set:\w+)')
-			RAIDVAR=$(echo $RAIDLINE | grep -oP '(?<=Random event set:)(\w+)')
-			RAIDTIME=$(echo $RAIDLINE | grep -oP '(\d{2}\/\d{2}\/\d{4}\s\d{2}\:\d{2}\:\d{2})')
-			if [[ $RAIDVAR != "" ]]
-			then
-				RAIDVAR=$(echo "${RAIDVAR##*$'\n'}")
-				if [[ $LASTRAID != $RAIDTIME ]]; then
-					RAIDMSG=${raids[$RAIDVAR]}
-					JSON='{"username": "RAID EVENT", "content": ":crossed_swords: RAID: '$RAIDMSG'"}'
-					echo "$LOGTIME Found $RAIDVAR and sent ${raids[$RAIDVAR]}" >> $LOGFILE
-					echo "Found $RAIDVAR and sent ${raids[$RAIDVAR]} from last raid"
-					curl --connect-timeout 10 -sSL -H "Content-Type: application/json" -X POST -d "$JSON" "$DISCORDWEBHOOK"
-					LASTRAID=$RAIDTIME
-				fi
-			fi
-			# RegEx to pull a Player Death line, parse player name, log time to prevent repeat messages, and send Discord message
-			DEATHLINE=$(echo $LATEST | grep -oP '(\d{2}\/\d{2}\/\d{4}\s\d{2}\:\d{2}\:\d{2}\:\s)(Got character ZDOID from )(.+?(?=:))(: 0:0)')
-			DEATHVAR=$(echo $DEATHLINE | grep -oP '(?<=Got character ZDOID from )(.+?(?= : 0:0))')
-			DEATHTIME=$(echo $DEATHLINE | grep -oP '(\d{2}\/\d{2}\/\d{4}\s\d{2}\:\d{2}\:\d{2})')
-			if [[ $DEATHVAR != "" ]]
-			then
-				echo "Death name is $DEATHVAR"
-				if [[ $LASTDEATH != $DEATHTIME ]]; then
-					LASTDEATH=$DEATHTIME
-					DEATHMSG=':headstone::skull_crossbones: '$DEATHVAR' has met their demise!'
-					DEATHJSON='{"username": "DEATH EVENT", "content": "'$DEATHMSG'"}'
-					echo "$LOGTIME Found $DEATHVAR and sent $DEATHMSG" >> $LOGFILE
-					echo "Found $DEATHVAR and sent $DEATHMSG"
-					curl --connect-timeout 10 -sSL -H "Content-Type: application/json" -X POST -d "$DEATHJSON" "$DISCORDWEBHOOK"
-					DEATHMSG=""
-					DEATHJSON=""
-				fi
-			fi
-			# RegEx to identify a new connection, get PlayFabID of new player and put into an array to handle multiple connections and player nicks
-			NEWCONNECT=$(echo $LATEST | grep -oP '(?<=Got character ZDOID from )(.+?(?=:)): (\S+(?=:))')
-			if [[ $NEWCONNECT != "" ]];
-			then
-				NEWNAME=$(echo $NEWCONNECT | grep -oP '.+?(?= :)')
-				PLAYFABID=$(echo $NEWCONNECT | grep -oP '(?<=: )(\S+)')
-				if [ ${players[$PLAYFABID]+_} ]; then
-					echo "User $PLAYFABID already exists"
-				else
-					players[${PLAYFABID}]=$NEWNAME
-					CONNECTMSG='{"username": "NEW VIKING", "content": ":crossed_swords: '${players[${PLAYFABID}]}' has joined."}'
-					curl --connect-timeout 10 -sSL -H "Content-Type: application/json" -X POST -d "$CONNECTMSG" "$DISCORDWEBHOOK"
-					CONNECTMSG=""
-					PLAYFABID=""
-					NEWNAME=""
-				fi
-			fi
-			# RegEx to identify player disconnect, send disconnect Discord message, and remove player name and PlayFabID from player array
-			DISCONNECT=$(echo $LATEST | grep -oP '(?<=Destroying abandoned non persistent zdo ).+?(?=:)')
-			if [[ $DISCONNECT != "" ]]
-			then
-				DISCONNECT=$(echo "${DISCONNECT##*$'\n'}")
-				echo "$LOGTIME PlayFabID $DISCONNECT ${players[$DISCONNECT]} disconnected" >> $LOGFILE
-				echo "PlayFabID $DISCONNECT ${players[$DISCONNECT]} disconnected"
-				if [ ${players[$DISCONNECT]+_} ]; then
-					DISCONNECTMSG='{"username": "TIL VALHALLA", "content": ":shield: '${players[$DISCONNECT]}' has disconnected."}'
-					curl --connect-timeout 10 -sSL -H "Content-Type: application/json" -X POST -d "$DISCONNECTMSG" "$DISCORDWEBHOOK"
-					unset players[$DISCONNECT]
-					DISCONNECTMSG=""
-				else
-					echo "$DISCONNECT not found"
-					echo "$LOGTIME $DISCONNECT not found" >> $LOGFILE
-				fi
-			fi
-			# Reset all event variables to null so we can test again, then sleep for 1 second
-			RAIDVAR=""
-			DEATHVAR=""
-			NEWCONNECT=""
-			DISCONNECT=""
-			LAST=$LATEST
+################################################################################
+# Event Processing Functions
+################################################################################
+
+process_raid_events() {
+	local latest="$1"
+	
+	# Extract raid information using original regex
+	local raid_line
+	raid_line=$(echo "$latest" | grep -oP '(\d{2}\/\d{2}\/\d{4}\s\d{2}\:\d{2}\:\d{2}\: Random event set:\w+)')
+	
+	if [[ -z "$raid_line" ]]; then
+		return
+	fi
+	
+	local raid_var raid_time
+	raid_var=$(echo "$raid_line" | grep -oP '(?<=Random event set:)(\w+)')
+	raid_time=$(echo "$raid_line" | grep -oP '(\d{2}\/\d{2}\/\d{4}\s\d{2}\:\d{2}\:\d{2})')
+	
+	# Prevent duplicate messages within same second
+	if [[ -z "${raid_last_time[$raid_var]}" ]] || [[ "${raid_last_time[$raid_var]}" != "$raid_time" ]]; then
+		local raid_msg="${raids[$raid_var]}"
+		if [[ -n "$raid_msg" ]]; then
+			send_discord "RAID EVENT" ":crossed_swords: RAID: $raid_msg"
+			raid_last_time[$raid_var]="$raid_time"
+			log_script "Raid detected: $raid_var"
 		fi
+	fi
+}
+
+process_death_events() {
+	local latest="$1"
+	
+	# Extract death information using original regex
+	local death_line
+	death_line=$(echo "$latest" | grep -oP '(\d{2}\/\d{2}\/\d{4}\s\d{2}\:\d{2}\:\d{2}\:\s)(Got character ZDOID from )(.+?(?=:))(: 0:0)')
+	
+	if [[ -z "$death_line" ]]; then
+		return
+	fi
+	
+	local death_var death_time
+	death_var=$(echo "$death_line" | grep -oP '(?<=Got character ZDOID from )(.+?(?=\s: 0:0))')
+	death_time=$(echo "$death_line" | grep -oP '(\d{2}\/\d{2}\/\d{4}\s\d{2}\:\d{2}\:\d{2})')
+	
+	if [[ -n "$death_var" ]]; then
+		# Prevent duplicate messages within same second
+		if [[ -z "${death_last_time[$death_var]}" ]] || [[ "${death_last_time[$death_var]}" != "$death_time" ]]; then
+			local death_msg=":headstone::skull_crossbones: $death_var has met their demise!"
+			send_discord "DEATH EVENT" "$death_msg"
+			death_last_time[$death_var]="$death_time"
+			log_script "Death detected: $death_var"
+		fi
+	fi
+}
+
+process_join_events() {
+	local latest="$1"
+	
+	# Extract connection information using original regex
+	local new_connect
+	new_connect=$(echo "$latest" | grep -oP '(?<=Got character ZDOID from )(.+?(?=:)): (\S+(?=:))')
+	
+	if [[ -z "$new_connect" ]]; then
+		return
+	fi
+	
+	local new_name playfab_id
+	new_name=$(echo "$new_connect" | grep -oP '.+?(?= :)')
+	playfab_id=$(echo "$new_connect" | grep -oP '(?<=: )(\S+)')
+	
+	# Check if player already tracked
+	if [[ -z "${players[$playfab_id]}" ]]; then
+		players[$playfab_id]="$new_name"
+		send_discord "NEW VIKING" ":crossed_swords: $new_name has joined."
+		log_script "Join detected: $new_name ($playfab_id)"
+	else
+		log_file_only "Player already tracked: $playfab_id -> $new_name"
+	fi
+}
+
+process_disconnect_events() {
+	local latest="$1"
+	
+	# Extract disconnect information using original regex
+	local disconnect
+	disconnect=$(echo "$latest" | grep -oP '(?<=Destroying abandoned non persistent zdo ).+?(?=:)')
+	
+	if [[ -z "$disconnect" ]]; then
+		return
+	fi
+	
+	# Get last match only
+	disconnect=$(echo "${disconnect##*$'\n'}")
+	
+	if [[ -n "${players[$disconnect]}" ]]; then
+		local player_name="${players[$disconnect]}"
+		send_discord "TIL VALHALLA" ":shield: $player_name has disconnected."
+		unset "players[$disconnect]"
+		log_script "Disconnect detected: $player_name ($disconnect)"
+	else
+		log_file_only "Disconnect warning: $disconnect not found in player array"
+	fi
+}
+
+################################################################################
+# Main Processing
+################################################################################
+
+process_log_changes() {
+	local latest
+	latest=$(tail -n 50 "$SERVERLOG")  # Increased from 10 to 50 lines for better event coverage
+	
+	# Use wc -l to detect actual changes (more reliable than string comparison)
+	local current_lines
+	current_lines=$(wc -l < "$SERVERLOG")
+	
+	if [[ $current_lines -le $last_processed_line ]]; then
+		return
+	fi
+	
+	log_file_only "Processing log lines from $last_processed_line to $current_lines"
+	
+	# Process all event types
+	process_raid_events "$latest"
+	process_death_events "$latest"
+	process_join_events "$latest"
+	process_disconnect_events "$latest"
+	
+	last_processed_line=$current_lines
+}
+
+main() {
+	log_script "Starting Valheim Discord Event Log"
+	
+	if [[ ! -e "$SERVERLOG" ]]; then
+		log_script "ERROR: Server log not found at $SERVERLOG - exiting"
+		exit 1
+	fi
+	
+	log_script "Found server log at $SERVERLOG"
+	last_processed_line=$(wc -l < "$SERVERLOG")
+	
+	# Main event loop
+	while true; do
+		process_log_changes
 		sleep 1
 	done
 }
 
-init
+################################################################################
+# Execution
+################################################################################
+
+main "$@"
